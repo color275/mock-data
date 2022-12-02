90f8abaac550b9675ff6a3a43f8ed3d106ae5a61
EC2 Default User <ec2-user@ip-10-10-101-56.ap-northeast-2.compute.internal>
2022-12-02 06:39:46 +0000

    modify

--- mock_gen.py
+++ mock_gen.py
@@ -2,7 +2,12 @@
 
 import configparser
 from faker import Faker
+from faker_vehicle import VehicleProvider
+
+
 fake = Faker('ko-KR')
+fake.add_provider(VehicleProvider)
+
 import cx_Oracle
 
 import datetime as dt
@@ -24,40 +29,60 @@ cnt = int(total_cnt/in_cnt)
 start_dt = dt.datetime(2021, 1, 1)
 start_sdt = '2021-01-01'
 
+def key_data(column_name, ind) :
+    prefix = ""
+    if column_name == "CST_NO" :
+        prefix = "C"
+    elif column_name == "ORD_NO" :
+        prefix = "R"
+    elif column_name == "PRD_CD" :
+        prefix = "P"
+
+    return prefix + str(ind).rjust(8,"0")        
+
 def f_fake_val(column_name, data_type, data_length, data_precision, data_scale, ind, seq) :    
     if column_name == pk :
-        return ind
+        return key_data(column_name, ind)
 
     if column_name == spk :
         return seq
     
     if data_type == "CLOB" and column_name == "PRD_INFO" :
-        text = ""
-        i = 0
-        while i < 100 :
-            text = text + fake.catch_phrase() 
-            i = i + 1        
-        return text
+        
+        text_size = fake.pyint(min_value=600, max_value=3000)
+        return fake.text(text_size)
 
     if data_type == 'DATE' :
         return fake.date_between(start_dt)
+
     if data_type == 'NUMBER' :
-        if column_name[-4:] == "_AMT" :
-            return round(fake.pyint(min_value=1000, max_value=1000000),-2)
+        if "AMT" in column_name :
+            return fake.pyint(min_value=1000, max_value=100000)
+        elif "COST" in column_name :
+            return fake.pyint(min_value=1000, max_value=100000)
+        elif "QTY" in column_name :
+            return fake.pyint(min_value=1, max_value=5)
         else :
             return fake.pyint(min_value=0, max_value=int("1".ljust(data_precision-data_scale,'0')))	
     
     if data_type[0:3] == 'VAR' :
-        if column_name == 'NAME' :
+        if column_name == 'CST_NM' :
             return fake.name()[0:data_length]        
-        elif pk != "CST_ID" and column_name == "CST_ID" :
-            return fake.pyint(min_value=1, max_value=100000)
-        elif pk != "PRD_ID" and column_name == "PRD_ID" :
-            return fake.pyint(min_value=1, max_value=100000)	            	            
+        elif pk != "CST_NO" and column_name == "CST_NO" :
+            ind = fake.pyint(min_value=1, max_value=total_cnt)
+            return key_data(column_name, ind)
+        elif pk != "PRD_CD" and column_name == "PRD_CD" :
+            ind = fake.pyint(min_value=1, max_value=total_cnt)
+            return key_data(column_name, ind)
+        elif pk != "ORD_NO" and column_name == "ORD_NO" :
+            ind = fake.pyint(min_value=1, max_value=total_cnt)
+            return key_data(column_name, ind)
         elif column_name == "IMG_PATH" :
             return fake.file_path(depth=3, category='image')
+        elif column_name[-3:] in ["_NM"] :
+            return (fake.vehicle_make() + " " + fake.vehicle_model() + " " + fake.vehicle_category())[0:data_length]
         elif column_name == "PRD_NM" :
-            return fake.catch_phrase()[0:data_length]        
+            return fake.vehicle_make() + " " + fake.vehicle_model() + " " + fake.vehicle_category()
         elif column_name == "ADDRESS" :
             return fake.address()
         elif column_name == "POSTCODE" :
@@ -66,8 +91,10 @@ def f_fake_val(column_name, data_type, data_length, data_precision, data_scale, 
             return fake.country()
         elif column_name == "JOB" :
             return fake.job()
-        elif column_name[-3:] == "_CD" :
-            return fake.text()[0:4].upper().replace(" ","")
+        elif column_name[-3:] in ["_CD"] :
+            return choice(['A', 'B', "C", "D", "E", "F"])
+        elif column_name[-4:] in ["_SCD","_DCD","_TCD"] :
+            return choice(['A', 'B', "C", "D", "E", "F"])
         elif column_name == "EMAIL" :
             return fake.email()
         elif column_name == "PHONE" :
@@ -79,6 +106,8 @@ def f_fake_val(column_name, data_type, data_length, data_precision, data_scale, 
         elif column_name[-4:] == "_SEQ" :
             return fake.pyint(min_value=0, max_value=4)	
         elif column_name[-3:] == "_ID" :
+            return fake.pyint(min_value=0, max_value=1000000)	
+        elif column_name[-3:] == "_NO`" :
             return fake.pyint(min_value=0, max_value=1000000)	
         elif column_name[-3:] == "_DT" :
             return fake.date_between(start_dt).strftime('%Y%m%d')
@@ -169,7 +198,7 @@ for i in range(1,cnt+1) :
         if spk == "NONE" :
             random_seq = 1
         else :
-            random_seq = random.randrange(1,8)
+            random_seq = random.choice([1,2,3])
         while seq <= random_seq :
             val = []
             for k, v in column_type.items() :
